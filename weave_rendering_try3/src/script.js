dc1f630c2b7f100bc6629eb99c1b87cae2cecddf
Laasya Kallepalli <lpkallepalli@gmail.com>
2024-10-22 12:15:33 -0400

    created a class for the warp and warps

--- weave_rendering_try3/src/script.js
+++ weave_rendering_try3/src/script.js
@@ -12,94 +12,202 @@ const basicWeave2DArray = [
 ];
 
 // --------------------------------------------------------------------------------
+// * classes * 
+// class for the weft noodle
+class weft {
+    constructor (arrayInput, zPos, weftMaterial) {
+        this.arrayInput = arrayInput;
+        this.zPos = zPos;
+        this.weftMaterial = weftMaterial;
+    }
+    // method to create the weft noodle mesh
+    creatingWeftNoodleMesh () {
+        const xPos = ((this.arrayInput[0].length)+0.2)/2;   // start xPos at num of warps + 0.2
+        const weftCurve = new THREE.CatmullRomCurve3([      // array points -> curve
+            new THREE.Vector3(-1*(xPos), 0, this.zPos),
+            new THREE.Vector3(xPos, 0, this.zPos)
+        ]);
+        // curve, geometry -> mesh
+        const weftMesh = new THREE.Mesh(
+            new THREE.TubeGeometry(weftCurve, 64, 0.1, 20, false), 
+            this.weftMaterial);
+        return weftMesh;                                    // return created weft noodle mesh
+    }
+}
+// class for creating group of weft noodles
+class wefts {
+    constructor (arrayInput, spacing, weftMaterial, scene) {
+        this.arrayInput = arrayInput;
+        this.spacing = spacing;
+        this.weftMaterial = weftMaterial;
+        this.scene = scene;
+    }
+    // method to add group of weft meshes to scene
+    addingWeftsToScene () {
+        let weftNum = this.arrayInput.length;               // get number of wefts from input array
+        let zPos;
+        if (weftNum % 2 === 0) { zPos = (-1*((weftNum-1)*(this.spacing/2))); }   // zPos for even number of weft noodles
+        else { zPos = (-1*(((Math.floor(weftNum/2))*this.spacing))); }           // zPos for odd number of weft noodles
+        let weftZpos = [];                                  // store all weft z positions
+        for (let i = 0; i < weftNum; i++) {                 // adding weft meshes from weft class to scene                    
+            const newWeft = new weft(this.arrayInput, zPos, this.weftMaterial); // making weft instance
+            const weftMesh = newWeft.creatingWeftNoodleMesh();                  // getting weft noodle Mesh from method
+            this.scene.add(weftMesh);                       // adding weft noodle mesh
+            weftZpos.push(zPos);                            // store each zPos of created weft to scene
+            zPos = zPos + this.spacing;                     // incrementing zPos by spacing
+        }
+    }
+    // method to get all of the zPos of the wefts
+    // getWeftsZPos() {
+
+    // }
+}
+// class for the warp noodle
+// class warp {
+//     constructor (warpRow, warpMaterial) {
+//         this.warpRow = warpRow;
+//         this. warpMaterial - warpMaterial;
+//     }
+//     // method to create warp noodle mesh
+//     creatingWarpNoodleMesh () {
+//         // let weftNum = arrayInput.length;   //TODO arrayInout param                     // get number of wefts from array
+//         // determining zPos of wefts
+//         // let zPos;
+//         // // even number of weft noodles
+//         // if (weftNum % 2 === 0) { zPos = (-1*((weftNum-1)*(spacing/2))); } 
+//         // // odd number of weft noodles
+//         // else { zPos = (-1*(((Math.floor(weftNum/2))*spacing))); }
+        
+//         // make array points depending on wefts pos
+//             // zpos = starts at start zpos of weft
+//                 // increments where the next weft is
+
+
+
+//         let zPosition = -1*((warpRow.length)/2);                // z pos start
+//         let warpArrayPoints = [];                               // initializing array to hold all of vec points
+//         let yPos = 0;                                           // initializing y pos
+//         let point;                                              // initializing point for array
+//         // for each element in the row -> array points
+//         for (let k = 0; k < warpRow.length; k++) {
+//             if (warpRow[k]==1) { yPos = 0.2; }                  // determine the y pos of point
+//             else { yPos = -0.2; }
+//             point = new THREE.Vector3(0, yPos, zPosition);      // put into z pos and y pos
+//             warpArrayPoints.push(point);                        // put point into array
+//             zPosition ++;                                       // incrementing z pos for next point
+//         }
+//         // array points -> curve
+//         const warpCurve = new THREE.CatmullRomCurve3(warpArrayPoints);
+//         // curve, geometry -> mesh
+//         const warpMesh = new THREE.Mesh(new THREE.TubeGeometry(warpCurve, 64, 0.1, 20, false), warpMaterial);
+
+//         return warpMesh;
+//     }
+// }
+
+// --------------------------------------------------------------------------------
 // * weft and warp functions * 
 // creating 1 yarn noodle weft
-function weft (arrayInput, zPos, weftMaterial) {
-    // xPos = length of weft noodles
-    const xPos = ((arrayInput[0].length)+((arrayInput[0].length)*0.1))/2;
-    // array pts -> curve
-    const weftCurve = new THREE.CatmullRomCurve3([
-        new THREE.Vector3(-1*(xPos), 0, zPos),
-        new THREE.Vector3(xPos, 0, zPos)
-    ]);
-    // curve, geometry -> mesh
-    const weftMesh = new THREE.Mesh(new THREE.TubeGeometry(weftCurve, 64, 0.1, 20, false), weftMaterial);
-    return weftMesh;
-}
-// creating all of the yarn noodle wefts
-function wefts (arrayInput, spacing, weftMaterial, scene) {
-    let weftNum = arrayInput.length;                        // get number of wefts from array
-    // determining zPos of wefts
-    let zPos;
-    // even number of weft noodles
-    if (weftNum % 2 === 0) { zPos = (-1*((weftNum-1)*(spacing/2))); } 
-    // odd number of weft noodles
-    else { zPos = (-1*(((Math.floor(weftNum/2))*spacing))); }
-    let weftMeshes = [];
-    // adding wefts to scene
-    for (let i = 0; i < weftNum; i++) {                      
-        const weftMesh = weft(arrayInput, zPos, weftMaterial);
-        scene.add(weftMesh);
-        weftMeshes.push(weftMesh);
-        zPos = zPos + spacing;                              // incrementing zPos
-    }
-    return weftMeshes;
-}
-// creating 1 yarn noodle warp
-function warp (warpRow, warpMaterial) {
-    let zPosition = -1*((warpRow.length)/2);                    // z pos start
-    let warpArrayPoints = [];                               // initializing array to hold all of vec points
-    let yPosition = 0;                                      // initializing y pos
-    let point;                                              // initializing point for array
-    // for each element in the row -> array points
-    for (let k = 0; k < warpRow.length; k++) {
-        if (warpRow[k]==1) { yPosition = 0.2; }                 // determine the y pos of point
-        else { yPosition = -0.2; }
-        point = new THREE.Vector3(0, yPosition, zPosition); // put into z pos and y pos
-        warpArrayPoints.push(point);                        // put point into array
-        zPosition ++;                                       // incrementing z pos for next point
-    }
-    // array points -> curve
-    const warpCurve = new THREE.CatmullRomCurve3(warpArrayPoints);
-    // curve, geometry -> mesh
-    const warpMesh = new THREE.Mesh(new THREE.TubeGeometry(warpCurve, 64, 0.1, 20, false), warpMaterial);
+// function weft (arrayInput, zPos, weftMaterial) {
+//     // xPos start at number of warps + 0.2
+//     const xPos = ((arrayInput[0].length)+0.2)/2;
+//     // array pts -> curve
+//     const weftCurve = new THREE.CatmullRomCurve3([
+//         new THREE.Vector3(-1*(xPos), 0, zPos),
+//         new THREE.Vector3(xPos, 0, zPos)
+//     ]);
+//     // curve, geometry -> mesh
+//     const weftMesh = new THREE.Mesh(new THREE.TubeGeometry(weftCurve, 64, 0.1, 20, false), weftMaterial);
+//     return weftMesh;
+// }
+// // creating all of the yarn noodle wefts
+// function wefts (arrayInput, spacing, weftMaterial, scene) {
+//     let weftNum = arrayInput.length;                        // get number of wefts from array
+//     // determining start zPos of wefts
+//     let zPos;
+//     // even number of weft noodles
+//     if (weftNum % 2 === 0) { zPos = (-1*((weftNum-1)*(spacing/2))); } 
+//     // odd number of weft noodles
+//     else { zPos = (-1*(((Math.floor(weftNum/2))*spacing))); }
+//     let weftMeshes = [];
+//     // adding wefts to scene
+//     for (let i = 0; i < weftNum; i++) {                      
+//         const weftMesh = weft(arrayInput, zPos, weftMaterial);
+//         scene.add(weftMesh);
+//         weftMeshes.push(weftMesh);
+//         zPos = zPos + spacing;                              // incrementing zPos
+//     }
+//     return weftMeshes;
+// }
+// // creating 1 yarn noodle warp
+// function warp (warpRow, warpMaterial) {
+//     // let weftNum = arrayInput.length;   //TODO arrayInout param                     // get number of wefts from array
+//     // determining zPos of wefts
+//     // let zPos;
+//     // // even number of weft noodles
+//     // if (weftNum % 2 === 0) { zPos = (-1*((weftNum-1)*(spacing/2))); } 
+//     // // odd number of weft noodles
+//     // else { zPos = (-1*(((Math.floor(weftNum/2))*spacing))); }
+    
+//     // make array points depending on wefts pos
+//         // zpos = starts at start zpos of weft
+//             // increments where the next weft is
 
-    return warpMesh;
-}
-// creating all of the yarn noodle wefts
-function warps (array, spacing, warpMaterial, scene) {
-    // transpose array
-    const transposedArray = array[0].map((_, colIndex) => array.map(row => row[colIndex]));
-    // determining xPos
-    let xPos;
-    if (transposedArray.length % 2 === 0) {                 // even warps
-        xPos = (-1*((transposedArray.length-1)*(spacing/2)));
-    }
-    else {                                                  // odd warps
-        xPos = (-1*(((Math.floor(transposedArray.length/2))*spacing)));
-    }
-    let warpMesh;                                           // initialize mesh
-    let warpMeshes = [];
-    // for each row in array, each warp
-    for (let i = 0; i < transposedArray.length; i++) {
-        warpMesh = warp(transposedArray[i], warpMaterial);  // make mesh
-        warpMesh.position.x = xPos;                         // change x pos
-        scene.add(warpMesh);                                // add to scene
-        xPos = xPos + spacing;                              // increment x pos for next warp
-        warpMeshes.push(warpMesh);
-    }
 
-    return warpMeshes;
-}
-// creates the wefts and warps
-function weave (arrayInput, spacing, weftMaterial, warpMaterial, scene) {
-    // create the weft and warp meshes
-    let weftMeshes = wefts(arrayInput, spacing, weftMaterial, scene);
-    let warpMeshes = warps(arrayInput, spacing, warpMaterial, scene);
 
-    // have the weftMeshe edges and warpMeshes edges matches in in rect prism
-    // create it at the orign
-}
+//     let zPosition = -1*((warpRow.length)/2);                // z pos start
+//     let warpArrayPoints = [];                               // initializing array to hold all of vec points
+//     let yPos = 0;                                           // initializing y pos
+//     let point;                                              // initializing point for array
+//     // for each element in the row -> array points
+//     for (let k = 0; k < warpRow.length; k++) {
+//         if (warpRow[k]==1) { yPos = 0.2; }                  // determine the y pos of point
+//         else { yPos = -0.2; }
+//         point = new THREE.Vector3(0, yPos, zPosition);      // put into z pos and y pos
+//         warpArrayPoints.push(point);                        // put point into array
+//         zPosition ++;                                       // incrementing z pos for next point
+//     }
+//     // array points -> curve
+//     const warpCurve = new THREE.CatmullRomCurve3(warpArrayPoints);
+//     // curve, geometry -> mesh
+//     const warpMesh = new THREE.Mesh(new THREE.TubeGeometry(warpCurve, 64, 0.1, 20, false), warpMaterial);
+
+//     return warpMesh;
+// }
+// // creating all of the yarn noodle wefts
+// function warps (array, spacing, warpMaterial, scene) {
+//     // transpose array
+//     const transposedArray = array[0].map((_, colIndex) => array.map(row => row[colIndex]));
+//     // determining xPos
+//     let xPos;
+//     if (transposedArray.length % 2 === 0) {                 // even warps
+//         xPos = (-1*((transposedArray.length-1)*(spacing/2)));
+//     }
+//     else {                                                  // odd warps
+//         xPos = (-1*(((Math.floor(transposedArray.length/2))*spacing)));
+//     }
+//     let warpMesh;                                           // initialize mesh
+//     let warpMeshes = [];
+//     // for each row in array, each warp
+//     for (let i = 0; i < transposedArray.length; i++) {
+//         warpMesh = warp(transposedArray[i], warpMaterial);  // make mesh
+//         warpMesh.position.x = xPos;                         // change x pos
+//         scene.add(warpMesh);                                // add to scene
+//         xPos = xPos + spacing;                              // increment x pos for next warp
+//         warpMeshes.push(warpMesh);
+//     }
+
+//     return warpMeshes;
+// }
+// // creates the wefts and warps
+// function weave (arrayInput, spacing, weftMaterial, warpMaterial, scene) {
+//     // create the weft and warp meshes
+//     let weftMeshes = wefts(arrayInput, spacing, weftMaterial, scene);
+//     let warpMeshes = warps(arrayInput, spacing, warpMaterial, scene);
+
+//     // have the weftMeshe edges and warpMeshes edges matches in in rect prism
+//     // create it at the orign
+// }
 
 // --------------------------------------------------------------------------------
 // * base * 
@@ -142,10 +250,14 @@ vertMaterial.roughness = 0.4
 const horizMaterial = new THREE.MeshStandardMaterial({color: 0x0044FF})
 horizMaterial.roughness = 0.4
 
-// adding wefts and warps to scene
-weave (
-    basicWeave2DArray, 1, 
-    horizMaterial, vertMaterial, scene);
+// using class to create weft group
+let weftsGroup = new wefts(basicWeave2DArray, 1, horizMaterial, scene);
+weftsGroup.addingWeftsToScene();
+
+// // adding wefts and warps to scene
+// weave (
+//     basicWeave2DArray, 1, 
+//     horizMaterial, vertMaterial, scene);
 
 // plane
 const plane = new THREE.Mesh(new THREE.PlaneGeometry(10, 10), planeMaterial)
